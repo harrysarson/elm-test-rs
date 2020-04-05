633859e3e0bb74706de7d9f9ed3d76ecd40a6088
Harry Sarson <harry.sarson@hotmail.co.uk>
2020-04-05 10:53:06 +0100

    format sexp before snapshotting

--- tree-sitter-elm/src/lib.rs
+++ tree-sitter-elm/src/lib.rs
@@ -9,23 +9,24 @@ pub fn language() -> tree_sitter::Language {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use tree_sitter::{Parser};
+    use insta::assert_display_snapshot;
     use std::fs;
     use std::path::PathBuf;
-    use insta::assert_debug_snapshot;
-    use itertools::Itertools;
+    use tree_sitter::Parser;
 
     #[test]
     fn test_language() {
         let mut parser = Parser::new();
         parser.set_language(language()).unwrap();
 
-        let source_file : PathBuf = ["c-src", "examples", "basic.elm"].iter().collect();
+        let source_file: PathBuf = ["c-src", "examples", "basic.elm"].iter().collect();
         let source_code = fs::read_to_string(source_file).unwrap();
 
         let tree = parser.parse(source_code, None).unwrap();
         let root_node = tree.root_node();
 
-        assert_debug_snapshot!(root_node.to_sexp());
+        let sexp = sexp::parse(&root_node.to_sexp()).expect("valid sexp");
+
+        assert_display_snapshot!(sexp.pretty());
     }
 }
